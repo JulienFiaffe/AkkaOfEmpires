e807f2e6cb519ddde6b8475baeae5a90fe052736
Julien Fiaffé <julien.fiaffe@live.fr>
2015-07-21 23:06:12 +0200

    Adding ResourcesSupervisorActor & FoodGathered message handler

---
+++ src/AkkaOfEmpires.Tests/Unit/Supervisor/ResourcesSupervisorActorTests.cs
@@ -0,0 +1,27 @@
+﻿using Akka.TestKit.VsTest;
+using AkkaOfEmpires.Domain.Messages.Gathering;
+using AkkaOfEmpires.Supervisor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace AkkaOfEmpires.Tests.Unit.Supervisor
+{
+    [TestClass]
+    public class ResourcesSupervisorActorTests : TestKit
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Shutdown();
+        }
+
+        [TestMethod]
+        public void When_FoodGathered_Received_Then_FoodAmount_Is_Increased()
+        {
+            var actor = ActorOfAsTestActorRef<ResourcesSupervisorActor>();
+            var message = new FoodGathered{Amount=10};
+            actor.Tell(message);
+            actor.UnderlyingActor.FoodAmount.ShouldBe<uint>(10);
+        }
+    }
+}
---
+++ src/AkkaOfEmpires/Supervisor/ResourcesSupervisorActor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Akka.Actor;
+using AkkaOfEmpires.Domain.Messages.Gathering;
+
+namespace AkkaOfEmpires.Supervisor
+{
+    public class ResourcesSupervisorActor : ReceiveActor
+    {
+        public UInt32 FoodAmount { get; private set; }
+        public UInt32 WoodAmount { get; private set; }
+        public UInt32 GoldAmount { get; private set; }
+        public UInt32 StoneAmount { get; private set; }
+
+        protected override void PreStart()
+        {
+            base.PreStart();
+            Become(SuperviseResources);
+        }
+
+        private void SuperviseResources()
+        {
+            Receive<FoodGathered>(m => OnFoodGathered(m));
+        }
+
+        private void OnFoodGathered(FoodGathered message)
+        {
+            FoodAmount += message.Amount;
+        }
+    }
+}
